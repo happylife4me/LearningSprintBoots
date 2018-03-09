3726ec0e388521d1ba592413606d1bf0a6e0e75f
Abhi Ram <happylife4me@gmail.com>
2018-03-09 12:42:41 +0530

    Day 4 class lunch

---
+++ spring-mvc-1/src/main/java/dom/demo/spring/WebSecurityInitialiizer.java
@@ -0,0 +1,7 @@
+package dom.demo.spring;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class WebSecurityInitialiizer extends AbstractSecurityWebApplicationInitializer {
+
+}
